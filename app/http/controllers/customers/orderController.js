28803cba20a2cbebf52566d88bc6b0b3da73e0fe
Binal Patel <binalpatel285@gmail.com>
2021-01-10 23:07:57 +0530

    single customer order status

--- app/http/controllers/customers/orderController.js
+++ app/http/controllers/customers/orderController.js
@@ -34,8 +34,18 @@ function orderController () {
                 res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, past-check=0, pre-check=0')
          
             res.render('customers/orders', { orders: orders, moment: moment })
+        },
+
+        async show(req, res) {
+          const order = await Order.findById(req.params.id)
+          // Authorize user
+          if(req.user._id.toString() === order.customerId.toString()) {
+             return res.render('customers/singleOrder', { order })
+          }
+             return res.redirect('/')
+          }
         }
     }
-}
+
 
 module.exports = orderController
